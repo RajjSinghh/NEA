d4d3e8c86b8680c8dfc51160127f6a8df76515ed
RajjSinghh <gurrajsingh22@gmail.com>
2020-03-17 20:28:05 +0000

    added tests for nn topologies

--- ai/nn.py
+++ ai/nn.py
@@ -7,13 +7,15 @@ xtest = tf.keras.utils.normalize(xtest, axis=1)
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+##model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+##model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+##model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+##model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+model.add(tf.keras.layers.Dense(16, activation=tf.nn.sigmoid))
+model.add(tf.keras.layers.Dense(16, activation=tf.nn.sigmoid))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
-
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"] )
-
+model.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=["accuracy"]  )
 model.fit(xtrain, ytrain, epochs=10)
 
 val_loss, val_acc = model.evaluate(xtest, ytest)
